7c6d2381e02795e6ff12f1058f6cc03c4148ff6b
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-02-26 12:32:44 -0600

    add slug methods to all models

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,12 @@
 class Brewery < ActiveRecord::Base
   has_many :beers
 
+  def slug
+    self.name.gsub(/[!?,'." "]/, '-').downcase
+  end
+
+  def self.find_by_slug(slug_name)
+    Brewery.all.detect {|brewery| brewery.slug == slug_name}
+  end
+
 end
