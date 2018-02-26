c5997254f624a4f204460e840a5615f33ac1012a
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-02-26 16:23:07 -0600

    post routes for login/signup created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,17 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect #homepage
+    else
+      #error message
+      redirect '/login'
+    end
+  end
+
   helpers do
     def logged_in?
       !!session[:user_id]
