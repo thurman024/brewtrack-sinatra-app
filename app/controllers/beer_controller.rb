624385e90d6a38583a0490be3935f80c8085711e
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-02-26 21:34:59 -0600

    post route for beers/edit added

--- app/controllers/beer_controller.rb
+++ app/controllers/beer_controller.rb
@@ -34,7 +34,7 @@ class BeerController < ApplicationController
       params[:beer][:user_id] = current_user.id
       Beer.create(params[:beer])
     end
-    redirect '/beers'
+    redirect "/beers/#{Beer.last.id}"
   end
 
   get '/beers/:id' do
@@ -53,10 +53,26 @@ class BeerController < ApplicationController
     end
   end
 
+  post '/beers/:id' do
+    @beer = Beer.find(params[:id])
+    if !params[:new_style].empty?
+      style = Style.create(name: params[:new_style])
+      params[:beer][:style_id] = style.id
+    end
+    if !params[:new_brewery].empty?
+      brewery = Brewery.create(name: params[:new_brewery], location: params[:location])
+      params[:beer][:brewery_id] = brewery.id
+    end
+    @beer.update(params[:beer])
+    redirect "/beers/#{@beer.id}"
+  end
+
   post '/beers/:id/delete' do
     if logged_in?
       @beer = Beer.find(params[:id])
       @beer.delete
     end
+    #success message
+    redirect '/beers'
   end
 end
