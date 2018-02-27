d0159f713f01cd2b7d4eb8af9c6f09b4f070e03e
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-02-26 19:45:37 -0600

    post new beer route added

--- app/controllers/beer_controller.rb
+++ app/controllers/beer_controller.rb
@@ -14,5 +14,25 @@ class BeerController < ApplicationController
     end
   end
 
-  post '/beers'
+  post '/beers' do
+    if params[:beer][:name].empty?
+      #raise error
+      redirect '/beers/new'
+    else
+      if !!params[:beer][:style_id] == !!params[:new_style]
+        #raise error, exactly one style input needed
+      elsif !params[:new_style].empty?
+        style = Style.create(name: params[:new_style])
+        params[:beer][:style_id] = style.id
+      end
+      if !!params[:beer][:brewery_id] == !!params[:new_brewery]
+        #raise error, exactly one brewery input needed
+      elsif !params[:new_brewery].empty?
+        brewery = Brewery.create(name: params[:new_brewery], location: params[:location])
+        params[:beer][:brewery_id] = brewery.id
+      end
+      params[:beer][:user_id] = current_user.id
+      Beer.create(params[:beer])
+    end
+  end
 end
